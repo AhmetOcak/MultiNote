508ba2533d9c1261dfb09e199fa47a8696bdfb6b
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2025-02-15 18:00:13 +0300

    - main activity implemented for the new settings

--- app/src/main/java/com/ahmetocak/multinote/MainActivity.kt
+++ app/src/main/java/com/ahmetocak/multinote/MainActivity.kt
@@ -4,18 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.getValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ahmetocak.multinote.features.add_new_note.AddNewNoteScreen
 import com.ahmetocak.multinote.core.ui.theme.MultiNoteTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().setKeepOnScreenCondition(condition = {
+            !viewModel.isPreferencesReady
+        })
         enableEdgeToEdge()
         setContent {
-            MultiNoteTheme {
-                AddNewNoteScreen()
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+            if (viewModel.isPreferencesReady) {
+                MultiNoteTheme(
+                    customColorScheme = uiState.colorScheme,
+                    dynamicColor = uiState.isDynamicColor,
+                    darkTheme = uiState.isDarkTheme
+                ) {
+                    AddNewNoteScreen()
+                }
             }
         }
     }
---
+++ app/src/main/java/com/ahmetocak/multinote/MainActivityViewModel.kt
@@ -0,0 +1,67 @@
+package com.ahmetocak.multinote
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ahmetocak.multinote.core.ui.theme.color_schemes.CustomColorScheme
+import com.ahmetocak.multinote.core.ui.theme.color_schemes.NeonColorScheme
+import com.ahmetocak.multinote.core.ui.theme.color_schemes.OrangeColorScheme
+import com.ahmetocak.multinote.core.ui.theme.color_schemes.PinkColorScheme
+import com.ahmetocak.multinote.core.ui.theme.color_schemes.SeaColorScheme
+import com.ahmetocak.multinote.core.ui.theme.color_schemes.YellowColorScheme
+import com.ahmetocak.multinote.data.repository.user_pref.UserPreferencesRepository
+import com.ahmetocak.multinote.model.ColorSchemeKeys
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class MainActivityViewModel @Inject constructor(
+    private val userPreferencesRepository: UserPreferencesRepository
+): ViewModel() {
+
+    private val _uiState = MutableStateFlow(MainUiState())
+    val uiState: StateFlow<MainUiState> = _uiState.asStateFlow()
+
+    init {
+        observeUserPreferences()
+    }
+
+    var isPreferencesReady by mutableStateOf(false)
+        private set
+
+    private fun observeUserPreferences() {
+        viewModelScope.launch(Dispatchers.IO) {
+            userPreferencesRepository.userPreferencesFlow.collect { preferences ->
+                _uiState.update {
+                    it.copy(
+                        isDarkTheme = preferences.isDarkTheme,
+                        isDynamicColor = preferences.isDynamicColor,
+                        colorScheme = when (preferences.colorScheme) {
+                            ColorSchemeKeys.YELLOW.name -> YellowColorScheme
+                            ColorSchemeKeys.NEON.name -> NeonColorScheme
+                            ColorSchemeKeys.ORANGE.name -> OrangeColorScheme
+                            ColorSchemeKeys.SEA.name -> SeaColorScheme
+                            else -> PinkColorScheme
+                        }
+                    )
+                }
+                isPreferencesReady = true
+            }
+        }
+    }
+}
+
+data class MainUiState(
+    val isDarkTheme: Boolean = false,
+    val isDynamicColor: Boolean = false,
+    val colorScheme: CustomColorScheme = PinkColorScheme
+)
